994ddd1cfe6e6ecdb538d53f712926263a5cd2e8
Stoyan Atanassov <stoyannn@gmail.com>
2019-04-16 17:00:19 -0400

    Create jijotest.ml

---
+++ jijo/jijotest.ml
@@ -0,0 +1,28 @@
+open Lexing
+open Jijoast
+open Jijoscan
+
+type ('a, 'b) result =
+  | Ok of 'a
+  | Error of 'b
+
+let str_of_pos_and_msg pos msg =
+  "Line " ^ (string_of_int pos.pos_lnum)
+  ^ ", character " ^ (string_of_int (pos.pos_cnum - pos.pos_bol + 1))
+  ^ ": " ^ msg
+
+let parse_buf lexbuf =
+  try
+    let program = Jijoparse.program Jijoscan.token lexbuf in
+    Ok (Jijohelp.str_of_program program)
+  with
+  | SyntaxError msg ->
+    Error (lexbuf.lex_curr_p, msg)
+  | Parsing.Parse_error ->
+    Error (lexbuf.lex_curr_p, "syntax error")
+
+let _ =
+  let lexbuf = Lexing.from_channel stdin in
+  match (parse_buf lexbuf) with
+  | Ok res -> print_endline res;
+  | Error (pos, msg) -> prerr_endline (str_of_pos_and_msg pos msg)
