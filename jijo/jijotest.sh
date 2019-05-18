f5b042675b21771141fa1389c9a9dad558899a94
user <user@debian.localdomain>
2019-05-18 17:04:56 -0400

    wip

--- jijo/jijotest.sh
+++ jijo/jijotest.sh
@@ -1,9 +1,16 @@
 #/bin/bash
+
 eval `opam config env`
-clang-6.0 -S -emit-llvm jijoruntime.c
+
+rm ./jijoruntime.ll
+rm ./tests/test.ll
+rm ./tests/test.native
 ocamlbuild -clean
-ocamlbuild -use-ocamlfind jijo.native -package llvm
+
+clang-6.0 -S -emit-llvm ./jijoruntime.c -o ./jijoruntime.ll
+ocamlbuild -use-ocamlfind ./jijo.native -package llvm
 ./jijo.native ./tests/test.jj > ./tests/test.ll
-chmod u+x ./tests/test.native
 llvm-link ./tests/test.ll ./jijoruntime.ll -o ./tests/test.native
+chmod u+x ./tests/test.native
+
 ./tests/test.native
